583230b4cace64cecec94476f57208a9de034ba6
513755565@qq.com <513755565@qq.com>
2020-12-13 10:09:36 +0800

    add route

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import App from './App.vue'
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  router,
+  el: "#app",
+  render: (h) => h(App),
+});
